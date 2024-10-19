353392f3ab68ef25954b03eb9cf4ccbdd03e052d
Farih Akmal Haqiqi <akmal@gamatecha.com>
2024-10-20 03:23:13 +0700

    nvim: integrate telescope with nvim-notify

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,10 +13,18 @@ return {
 		table.insert(vimgrep_arguments, '--glob')
 		table.insert(vimgrep_arguments, '!**/.git/*')
 
+		require('telescope').load_extension('notify')
+
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
 		vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = 'Telescope live grep' })
 		vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope buffers' })
 		vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = 'Telescope help tags' })
+		vim.keymap.set(
+			'n',
+			'<leader>pn',
+			function() vim.cmd('Telescope notify') end,
+			{ desc = 'Telescope notifications' }
+		)
 
 		require('telescope').setup({
 			defaults = {
