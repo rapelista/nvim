f4f76f91ddc244beab6dd0de9e6dc08ee63b90c8
Farih Akmal Haqiqi <farih.282828@gmail.com>
2026-06-29 17:59:17 +0700

    add autotag plugins

---
+++ lua/plugins/autotag.lua
@@ -0,0 +1,18 @@
+return {
+  "windwp/nvim-ts-autotag",
+  event = { "BufReadPre", "BufNewFile" },
+  ft = {
+    "html",
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "tsx",
+    "jsx",
+    "xml",
+    "vue",
+    "svelte",
+    "markdown",
+  },
+  opts = {},
+}
